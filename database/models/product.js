2adce504eeec962bb8372e0f06f505e7ad77cb78
YuliiaShulimenko <shulichka4@gmail.com>
2023-11-13 14:06:30 +0100

    deploy

---
+++ database/models/product.js
@@ -0,0 +1,22 @@
+const sequelize = require('../database');
+const { DataTypes } = require("sequelize");
+const { Sequelize } = require('sequelize');
+const Category = require('./category');
+
+const Product = sequelize.define("product", {
+    id : {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: DataTypes.TEXT,
+    price: DataTypes.INTEGER,
+    discont_price: DataTypes.INTEGER,
+    description: DataTypes.TEXT,
+    image: DataTypes.TEXT,
+    rating: DataTypes.INTEGER,
+});
+
+
+
+module.exports = Product;
